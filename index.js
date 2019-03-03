9ee828051bf089c41a6296ef141a70a33df4d29e
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-03-03 17:09:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+let players = require("./players.json");
 
 
 fs.readdir("./commands/", (err, files) => {
