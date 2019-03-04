299c0638d65bd1df6e9baa8b247cc95df129e4c7
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-03-03 21:43:57 -0500

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -17,17 +17,33 @@ module.exports.run = async (bot, message, args) => {
     let uAG = stats[maid].agility;
     let uAL = stats[maid].alertness;
     let uLUCK = stats[maid].luck;
+    let uFOR = stats[maid].foraging;
 
     //return message.reply(`you have:\n\n💰 Coins: ${uCoins}\n\n🏦 Bank: ${uBank}\n\n⛹ Net Worth: ${uNet}`)
     let balEmbed = new Discord.RichEmbed()
     .setColor('#2ecc71')
-    .setDescription(`**<@${maid}>**\nAttack: ${uAT}\nDefence: ${uDEF}\nLooting: ${uLOOT}\nFishing: ${uFISH}\nAgility: ${uAG}\nAlertness: ${uAL}\nLuck: ${uLUCK}`);
+    .setDescription(`**<@${maid}>**\nAttack: ${uAT}\nDefence: ${uDEF}\nLooting: ${uLOOT}\nFishing: ${uFISH}\nForaging: ${uFOR}\nAgility: ${uAG}\nAlertness: ${uAL}\nLuck: ${uLUCK}`);
     
     return message.channel.send(balEmbed)
   }
   if(!stats[cUser.id]) {
     return message.channel.send(`It looks like ${cUser} has not started playing yet.`);
   }
+  let pAT = stats[cUser.id].attack;
+  let pDEF = stats[cUser.id].defence;
+  let pFISH = stats[cUser.id].fishing;
+  let pLOOT = stats[cUser.id].looting;
+  let pAG = stats[cUser.id].agility;
+  let pAL = stats[cUser.id].alertness;
+  let pLUCK = stats[cUser.id].luck;
+  let pFOR = stats[cUser.id].foraging;
+
+    //return message.reply(`you have:\n\n💰 Coins: ${uCoins}\n\n🏦 Bank: ${uBank}\n\n⛹ Net Worth: ${uNet}`)
+    let balEmbed = new Discord.RichEmbed()
+    .setColor('#2ecc71')
+    .setDescription(`**<@${maid}>**\nAttack: ${pAT}\nDefence: ${pDEF}\nLooting: ${pLOOT}\nFishing: ${pFISH}\nForaging: ${pFOR}\nAgility: ${pAG}\nAlertness: ${pAL}\nLuck: ${pLUCK}`);
+    
+    return message.channel.send(balEmbed)
   
 }
 
