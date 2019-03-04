a07f35d4be88f5835296351e260c3b32648befe1
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2019-03-03 22:55:04 -0500

    Create inv.js

---
+++ commands/inv.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+let stats = require("../stats.json");
+let inv = require("../inv.json");
+
+
+module.exports.run = async (bot, message, args) => {
+  let maid = message.author.id;
+    let balEmbed = new Discord.RichEmbed()
+    .setColor('#2ecc71')
+    .setDescription(`**<@${message.author.id}>'s Inventory:**\nWheat: ${inv[maid].wheat}\nEggs: ${inv[maid].egg}\nFruit: ${inv[maid].fruit}\nRoot: ${inv[maid].root}\nSticks: ${inv[maid].sticks}\nForaging Lootbag: ${inv[maid].foragelootbag}`);
+    
+    message.channel.send("I am privately messaging you your inventory.");
+    return message.user.send(balEmbed)
+}
+
+module.exports.help = {
+  name: "inv"
+}
